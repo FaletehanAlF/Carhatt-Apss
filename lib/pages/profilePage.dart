259446201657e2ae9c78cda7a6da5dbb1d3096bf
FaletehanAlF <faletehanalfarabi09@gmail.com>
2026-09-04 21:30:15 +0700

    update code

--- lib/pages/profilePage.dart
+++ lib/pages/profilePage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import './login_page.dart';
+import './loginPage.dart';
 import './registerPage.dart';
 
 class ProfilePage extends StatelessWidget {
